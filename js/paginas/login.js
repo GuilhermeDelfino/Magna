1cd6d80fc8cf68d0de727708f18e1280b9075915
pedroraneafreitas <pedro.freitas@sptech.school>
2022-04-15 16:44:18 -0300

    Pagina de login, CSS, HTML e JS para validação

--- js/paginas/login.js
+++ js/paginas/login.js
@@ -1,5 +1,19 @@
-function validar(){
+function validar() {
       var email = inp_email.value
       var senha = inp_senha.value
 
+      
+       if (email == '') {
+            erro.innerHTML = `Email vazio.`
+      }else if( senha == ''){
+            erro.innerHTML = `Senha vazia.`
+      }else if (email.indexOf('@') == -1) {
+            erro.innerHTML = `Email, invalido.`
+      } else if (email.length < 4){
+            erro.innerHTML =`Email`
+      }else if (senha.length < 4){
+
+      }else if(email.toLowerCase() =="fernando.brandao@sptech.school" && senha =="12345678"){
+              window.location = "dashboard.html"
+      }
 }
