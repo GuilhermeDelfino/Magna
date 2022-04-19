76139794b646e10cdfc276d3d157db8401bd436b
pedroraneafreitas <pedro.freitas@sptech.school>
2022-04-18 21:32:11 -0300

    corrigindo erros do CSS e JS

--- js/paginas/login.js
+++ js/paginas/login.js
@@ -4,16 +4,24 @@ function validar() {
 
       
        if (email == '') {
-            erro.innerHTML = `Email vazio.`
+            erro.innerHTML = `E-mail vazio.`
+             erro.style.padding='8px'
       }else if( senha == ''){
             erro.innerHTML = `Senha vazia.`
+            erro.style.padding='8px'
       }else if (email.indexOf('@') == -1) {
-            erro.innerHTML = `Email, invalido.`
+            erro.innerHTML = `E-mail, invalido.`
+            erro.style.padding='8px'
       } else if (email.length < 4){
-            erro.innerHTML =`Email`
+            erro.innerHTML =`E-mail deve possuir 4 ou mais caracteres.`
+            erro.style.padding='8px'
       }else if (senha.length < 4){
+            erro.innerHTML =`Senha deve possuir 4 ou mais caracteres.`
+            erro.style.padding='8px'
 
       }else if(email.toLowerCase() =="fernando.brandao@sptech.school" && senha =="12345678"){
               window.location = "dashboard.html"
+
+              
       }
 }
