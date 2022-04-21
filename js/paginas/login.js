516ff2f0a9fb72363a9ed395d8ab12cb7fdad64a
Delfino <guilherme.narciso@sptech.school>
2022-04-21 15:28:03 -0300

    :twisted_rightwards_arrows: Merge Login Pedro

--- js/paginas/login.js
+++ js/paginas/login.js
@@ -1,27 +1,44 @@
-function validar() {
-      var email = inp_email.value
-      var senha = inp_senha.value
+function validar () {
+      var email = inp_email.value.trim();
+      var senha = inp_senha.value.trim();
+      if (email == '')
+      {
+            erro.innerHTML = `Preencha o E-mail`;
+            erro.className = 'erro';
+      } else if (senha == '')
+      {
+            erro.innerHTML = `Preencha a senha`;
+            erro.className = 'erro';
+      } else if (email.indexOf('@') == -1)
+      {
+            erro.innerHTML = `Seu e-mail está invalido.`;
+            erro.className = 'erro';
+      } else if (email.length < 4)
+      {
+            erro.innerHTML = `E-mail deve possuir 4 ou mais caracteres.`;
+            erro.className = 'erro';
+      } else if (senha.length < 4)
+      {
+            erro.innerHTML = `Senha deve possuir 4 ou mais caracteres.`;
+            erro.className = 'erro';
 
-      
-       if (email == '') {
-            erro.innerHTML = `E-mail vazio.`
-             erro.style.padding='8px'
-      }else if( senha == ''){
-            erro.innerHTML = `Senha vazia.`
-            erro.style.padding='8px'
-      }else if (email.indexOf('@') == -1) {
-            erro.innerHTML = `E-mail, invalido.`
-            erro.style.padding='8px'
-      } else if (email.length < 4){
-            erro.innerHTML =`E-mail deve possuir 4 ou mais caracteres.`
-            erro.style.padding='8px'
-      }else if (senha.length < 4){
-            erro.innerHTML =`Senha deve possuir 4 ou mais caracteres.`
-            erro.style.padding='8px'
-
-      }else if(email.toLowerCase() =="fernando.brandao@sptech.school" && senha =="12345678"){
-              window.location = "dashboard.html"
-
-              
+      } else if (email.toLowerCase() == "fernando.brandao@sptech.school" && senha == "12345678")
+      {
+            window.location = "dashboard.html";
+      } else
+      {
+            erro.innerHTML = `Usuário não encontrato`;
+            erro.className = 'erro';
+            setTimeout(() => {
+                  limparCampos();
+            }, 2000);
       }
+}
+
+function limparCampos () {
+      inp_senha.value = '';
+      inp_email.value = '';
+      erro.innerHTML = '';
+      erro.className = 'd-none';
+      erro.style.padding = '0';
 }
